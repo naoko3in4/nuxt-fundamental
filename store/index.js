84ed55b118972383ae3af1c65a39934431e5c918
Naoko3in4 <okashimochi@gimail.com>
2020-04-26 18:02:37 +0900

    created posts.js and put all posts

---
+++ store/index.js
@@ -0,0 +1,5 @@
+
+export const status = () => ({
+  
+})
+export const getter = {}
---
+++ store/posts.js
@@ -0,0 +1,19 @@
+export const state = () => ({
+  all: [
+    {
+      id: 'balut',
+      title: 'What is Balut?',
+      content: 'If someone placed balut on your plate, you might think they were serving you a hardboiled egg.'
+    },
+    {
+      id: 'whereIsIt',
+      title: 'Where is the signs in button??',
+      content: 'I\'ve stalked the forum for a while, reading everything closely, but today I decided that it was time to register as a user. So I did'
+    },
+    {
+      id: 'how',
+      title: 'How can I fix this problem?',
+      content: 'Please help me. I don\'t know what is the best way to fix my problem. I cant reproduce and I cant tell more info. Can someone help me asap?'
+    },
+  ]
+})
